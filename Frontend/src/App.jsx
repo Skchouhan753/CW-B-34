1c04d56ff34fef0e31fd833c3df3e5796d1d6c84
Skchouhan753 <s.k.chouhan753@gmail.com>
2024-03-31 16:43:38 +0530

    V 1.0

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import AllRoutes from "./AllRoutes/AllRoutes";
+import Footer from "./Components/Footer/Footer";
+
+
+
+
+
+function App() {
+  return (
+    <>
+      <div>
+
+      <AllRoutes/>
+       
+        <Footer/>
+      </div>
+    </>
+  );
+}
+
+export default App;
